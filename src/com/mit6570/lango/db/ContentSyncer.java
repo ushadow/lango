03de12548c9e0cea29ae1422cd270df9f326feb3
yingyin <clevershadow@gmail.com>
2013-11-06 00:36:53 -0500

    Added back database files.

---
+++ src/com/mit6570/lango/db/ContentSyncer.java
@@ -0,0 +1,58 @@
+package com.mit6570.lango.db;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Collection;
+
+import net.epsilonlabs.datamanagementefficient.library.DataManager;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
+import android.util.Log;
+
+/**
+ * Syncs with the course web site asynchronously.
+ * @author yingyin
+ *
+ */
+public class ContentSyncer extends AsyncTask<String, Void, Void>{
+  private DataManager dm;
+  
+  public ContentSyncer(Context context) {
+    dm = DataManager.getInstance(context);
+    dm.open();
+  }
+  
+  @Override
+  protected Void doInBackground(String... args) {
+    Collection<Course> courses = dm.find(Course.class, "title", args[0]);
+    //if (courses.isEmpty()) {
+      CourseParser.parseAndCreate(args[1]);
+    //}
+    dm.close();
+    return null;
+  }
+  
+  private boolean needUpdate(byte[] digest, byte[] oldDigest) {
+    Log.i("langlo", Arrays.toString(digest));
+    Log.i("lango", Arrays.toString(oldDigest));
+    return oldDigest == null || Arrays.equals(digest, oldDigest);
+  }
+  
+  private void updateCourse(Document doc) {
+    Log.i("lango", "update course");
+  }
+}
---
+++ src/com/mit6570/lango/db/CourseDbHelper.java
@@ -0,0 +1,80 @@
+package com.mit6570.lango.db;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class CourseDbHelper extends SQLiteOpenHelper{
+  public static final int DATABASE_VERSION = 1;
+  public static final String DB_PATH = "/data/data/com.mit6570.lango/databases/";
+  public static final String DB_NAME = "course.db";
+  
+  private static final String SQL_DELETE_TABLES = 
+      "DELETE FROM sqlite_master WHERE TYPE in ('table', 'index', 'triger')";
+  
+  private Context context;
+  
+  public CourseDbHelper(Context context) {
+    super(context, DB_NAME, null, DATABASE_VERSION);
+    this.context = context;
+  }
+  
+  public void createDatabase() {
+    boolean dbExist = checkDatabase();
+    if (!dbExist) {
+      getReadableDatabase();
+      close();
+      try {
+        copyDatabase();
+      } catch (IOException ioe) {
+        ioe.printStackTrace();
+      }
+    }
+  }
+  
+  public void onCreate(SQLiteDatabase db) {}
+
+  @Override
+  public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    db.execSQL(SQL_DELETE_TABLES);
+    onCreate(db);
+  }
+  
+  public Cursor queryCourse(SQLiteDatabase db) {
+    String[] columns = {
+       CourseContract.Course.COLUMN_NAME_TITLE,
+       CourseContract.Course.COLUMN_NAME_URL,
+       CourseContract.Course.COLUMN_NAME_HASH
+    };
+    
+    return db.query(CourseContract.Course.TABLE_NAME, columns, null, null, null, null, null);
+  }
+  
+  private boolean checkDatabase() {
+    String path = DB_PATH + DB_NAME;
+    File f = new File(path);
+    return f.exists();
+  }
+  
+  private void copyDatabase() throws IOException {
+    InputStream is = context.getAssets().open(DB_NAME);
+    String path = DB_PATH + DB_NAME;
+    OutputStream os = new FileOutputStream(path);
+    
+    byte[] buffer = new byte[1024];
+    int length;
+    while ((length = is.read(buffer)) > -1) {
+      os.write(buffer, 0, length);
+    }
+    os.flush();
+    os.close();
+    is.close();
+  }
+}
